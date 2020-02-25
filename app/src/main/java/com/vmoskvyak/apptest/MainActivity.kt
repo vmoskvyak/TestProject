8f3add81d8ca1de27469175379d350acf1e681f5
Volodymyr Moskvyak <volodymyr.moskvyak@2denker.de>
2020-02-25 16:45:07 +0100

    first commit

---
+++ app/src/main/java/com/vmoskvyak/apptest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vmoskvyak.apptest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
